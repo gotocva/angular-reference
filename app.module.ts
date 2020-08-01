3a5053e9f553aef14f5a7790aa1344b4a4e76cf4
Sivabharathy <gotocva@gmail.com>
2020-08-01 11:46:32 +0530

    app module created

---
+++ app.module.ts
@@ -0,0 +1,14 @@
+
+import { httpInterceptorProviders } from "./interceptor";
+
+@NgModule({
+    declarations: [],
+    imports: [],
+    providers: [httpInterceptorProviders], 
+    bootstrap: [],
+  })
+export class AppModule {}
+
+/**
+ *  sample app.module to explain how to import all things
+ */
